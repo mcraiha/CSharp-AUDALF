277c128b346a583ffc61c6377c0e9c08d6c4cd0f
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2020-06-20 17:22:33 +0300

    Add samples folder for deserialization tests
    Since there will be many files

--- tests/DeserializeTests.cs
+++ tests/DeserializeTests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using CSharp_AUDALF;
 using System;
+using System.IO;
 using System.Collections.Generic;
 
 namespace Tests
@@ -48,6 +49,40 @@ namespace Tests
 											/* VALUE TYPE ID #5 */ 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
 											/* ACTUAL VALUE #5 */ 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 
 											};
+			byte[] expected = new byte[] { 0, 1, 10, 100, 255 };
+
+			// Act
+			bool isAUDALF = AUDALF_Deserialize.IsAUDALF(inputArray);
+			uint versionNumber = AUDALF_Deserialize.GetVersionNumber(inputArray);
+			ulong byteSize = AUDALF_Deserialize.GetByteSize(inputArray);
+			bool isDictionary = AUDALF_Deserialize.IsDictionary(inputArray);
+			ulong indexCount = AUDALF_Deserialize.GetIndexCount(inputArray);
+			ulong[] entryDefinitionOffsets = AUDALF_Deserialize.GetEntryDefinitionOffsets(inputArray);
+			byte[] byteArray = AUDALF_Deserialize.Deserialize<byte>(inputArray);
+
+			// Assert
+			Assert.IsTrue(isAUDALF, "Result should be AUDALF payload");
+			Assert.AreEqual(BitConverter.ToUInt32(Definitions.versionNumber, 0), versionNumber, "Result should have correct version number");
+			Assert.IsFalse(isDictionary, "Result should contain an array, not a dictionary");
+			Assert.AreEqual((ulong)byteArray.LongLength, indexCount, "Result should contain certain number of items");
+			Assert.AreEqual(indexCount, (ulong)entryDefinitionOffsets.LongLength, "Result should have certain number of entry definitions");
+			
+			foreach (ulong u in entryDefinitionOffsets)
+			{
+				Assert.GreaterOrEqual(u, (ulong)Definitions.entryDefinitionsOffset, "Each entry definition should point to valid address inside the payload");
+				Assert.LessOrEqual(u, byteSize, "Each entry definition should point to valid address inside the payload");
+				Assert.IsTrue(u % 8 == 0, "Every offset should align to 8 bytes (64 bits)");
+			}
+
+			Assert.AreEqual(byteSize, inputArray.LongLength);
+			CollectionAssert.AreEqual(expected, byteArray);
+		}
+
+		[Test]
+		public void DeserializeAUDALFFileToByteArray()
+		{
+			// Arrange
+			byte[] inputArray = File.ReadAllBytes("samples/bytes_0_1_10_100_255.audalf");
 			byte[] expected = new byte[] { 0, 1, 10, 100, 255 };
 
 			// Act
